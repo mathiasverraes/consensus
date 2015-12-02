ce5298d931fa0bab1dcc47575cbccdded5cb1edc
Mathias Verraes <mathias@verraes.net>
2015-12-02 08:53:55 +0100

    put

--- lib/consensus/kvs.ex
+++ lib/consensus/kvs.ex
@@ -16,14 +16,22 @@ defmodule Consensus.Kvs do
     GenServer.cast(pid, {:put, key, value})
   end
 
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
 # Callbacks
 
   def init(_opts) do
     {:ok, %Consensus.Kvs{}}
   end
 
-  def handle_call(_msg, _from, state) do
-    {:reply, :ok, state}
+  def handle_call({:get, key}, _from, state) do
+    {
+      :reply,
+      Map.get(state.values, key),
+      state
+    }
   end
 
   def handle_cast({:put, key, value}, state) do
